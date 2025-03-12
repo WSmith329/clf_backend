b2167bc0d0b0da51fa0e86897053785bc436b8c4
William Smith <w.smith@k-tech.it>
2025-03-12 08:54:53 +0000

    public

--- clf_backend/settings.py
+++ clf_backend/settings.py
@@ -126,7 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = env('STATIC_ROOT')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = env('STATIC_ROOT')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
